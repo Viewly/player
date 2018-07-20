28885a3afc38d96ff977a9ea526c99416f0329a2
Boris Vujicic <turshija@gmail.com>
2018-07-20 13:06:11 +0200

    return cache invalidation in deploy-dev.sh script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,7 +3,7 @@ npm run build
 if [ -d "build" ]; then
   cd build
   aws s3 sync . s3://viewly-player-dev
-  # aws cloudfront create-invalidation --distribution-id XXXXXXXXXXX --paths /index.html
+  aws cloudfront create-invalidation --distribution-id E1QZAKZNN6JR1G --paths /index.html
 fi
 
 while true; do echo 'sleeping'; sleep 3600; done
